1c525949128f8a26a4583f682e832604673da663
aleksandr <asdiamond@miners.utep.edu>
2018-11-18 22:31:32 -0700

    Got producer consumer working with an end signal and only a single buffer.

--- producer_consumer.py
+++ producer_consumer.py
@@ -5,7 +5,13 @@ Credit goes to: https://gist.github.com/mahdavipanah/b8b7999e9542458a9b908112c1e
 """
 
 import threading
+import time
+import random
 
+random.seed(0)
+
+stuff = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+index = 0
 
 # Buffer size
 N = 10
@@ -13,14 +19,22 @@ N = 10
 # Buffer init
 buf = [0] * N
 
-
 fill_count = threading.Semaphore(0)
 empty_count = threading.Semaphore(N)
 
 
 def produce():
-    print("One item produced!")
-    return 1
+    global index
+    # return whatever is produced
+    delay = random.uniform(1, 2)
+    if index < len(stuff):
+        k = stuff[index]
+        index += 1
+    else:  # we've produced everything, send signal to stop
+        k = None
+    time.sleep(delay)
+    print(f'Produced {k} in {delay} seconds')
+    return k
 
 
 def producer():
@@ -31,10 +45,15 @@ def producer():
         buf[front] = x
         fill_count.release()
         front = (front + 1) % N
+        if x is None:
+            return
 
 
 def consume(y):
-    print("One item consumed!")
+    # do whatever you need to to consume 'y'
+    delay = random.uniform(1, 2)
+    time.sleep(delay)
+    print(f"Consumed {y} in {delay} seconds")
 
 
 def consumer():
@@ -45,6 +64,8 @@ def consumer():
         empty_count.release()
         consume(y)
         rear = (rear + 1) % N
+        if y is None:
+            return
 
 
 producer_thread = threading.Thread(target=producer)
